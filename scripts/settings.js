3c9ec2cc8b86e9290fd3ed5b3fa0170166f24b74
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-03-21 12:07:26 -0500

    removed redundancy

--- scripts/settings.js
+++ scripts/settings.js
@@ -67,8 +67,6 @@ export const LookfarSettings = {
       type: Boolean,
       default: true
     });
-
-    LookfarSettings.registerDynamicRollTableSettings();
   },
 
   registerDynamicRollTableSettings() {
