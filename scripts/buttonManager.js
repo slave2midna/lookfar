be14cc713e333cf72df1defc4af132eaf95f87a3
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-03-21 08:43:21 -0500

    commented out treasure

--- scripts/buttonManager.js
+++ scripts/buttonManager.js
@@ -20,22 +20,21 @@ Hooks.once("ready", () => {
     console.log("Travel Check button added to the toolbar:", tools);
   });
 
-  // Adds floating button for Treasure Roll into Project FU's toolbar
-  Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
-    console.log("Adding Treasure Roll button to toolbar...");
-    
-    let treasureRollButton = {
-      name: "Treasure Roll",
-      title: "Generate Some Treasure",
-      icon: "fa-solid fa-gem",
-      button: true,
-      onClick: () => {
-        console.log("Treasure Roll button clicked!");
-        Hooks.call("lookfarShowTravelCheckDialog"); // Placeholder function
-      },
-      visible: true
-    };
-    tools.push(treasureRollButton);
-    console.log("Treasure Roll button added to the toolbar:", tools);
-  });
-});
+// // Adds floating button for Treasure Roll into Project FU's toolbar
+// Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
+//   console.log("Adding Treasure Roll button to toolbar...");
+  
+//   let treasureRollButton = {
+//     name: "Treasure Roll",
+//     title: "Generate Some Treasure",
+//     icon: "fa-solid fa-gem",
+//     button: true,
+//     onClick: () => {
+//       console.log("Treasure Roll button clicked!");
+//       Hooks.call("lookfarShowTravelCheckDialog"); // Placeholder function
+//     },
+//     visible: true
+//   };
+//   tools.push(treasureRollButton);
+//   console.log("Treasure Roll button added to the toolbar:", tools);
+// });
