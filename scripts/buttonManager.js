2edf4e820ad96bce7c1c9dee6b3cbbd94975cd1f
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-03-21 15:21:20 -0500

    fixed button placement issue

--- scripts/buttonManager.js
+++ scripts/buttonManager.js
@@ -4,8 +4,8 @@ Hooks.once("ready", () => {
   // Adds floating button for Travel Check into Project FU's toolbar
   Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
     console.log("Adding Travel Check button to toolbar...");
-    
-    let travelCheckButton = {
+
+    const travelCheckButton = {
       name: "Travel Check",
       title: "Make a Travel Check",
       icon: "fa-solid fa-person-hiking",
@@ -14,28 +14,36 @@ Hooks.once("ready", () => {
         console.log("Travel Check button clicked!");
         Hooks.call("lookfarShowTravelCheckDialog");
       },
-      visible: true
+      visible: true,
+      sort: 9999
     };
+
     tools.push(travelCheckButton);
+    tools.sort((a, b) => (a.sort || 0) - (b.sort || 0));
     console.log("Travel Check button added to the toolbar:", tools);
   });
 
-// // Adds floating button for Treasure Roll into Project FU's toolbar
-// Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
-//   console.log("Adding Treasure Roll button to toolbar...");
-  
-//   let treasureRollButton = {
-//     name: "Treasure Roll",
-//     title: "Generate Some Treasure",
-//     icon: "fa-solid fa-gem",
-//     button: true,
-//     onClick: () => {
-//       console.log("Treasure Roll button clicked!");
-//       Hooks.call("lookfarShowTravelCheckDialog"); // Placeholder function
-//     },
-//     visible: true
-//   };
-//   tools.push(treasureRollButton);
-//   console.log("Treasure Roll button added to the toolbar:", tools);
-// });
+  // Uncomment this section when the Treasure Roll feature is ready
+  /*
+  Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
+    console.log("Adding Treasure Roll button to toolbar...");
+
+    const treasureRollButton = {
+      name: "Treasure Roll",
+      title: "Generate Some Treasure",
+      icon: "fa-solid fa-gem",
+      button: true,
+      onClick: () => {
+        console.log("Treasure Roll button clicked!");
+        Hooks.call("lookfarShowTravelCheckDialog"); // Placeholder
+      },
+      visible: true,
+      sort: 10000
+    };
+
+    tools.push(treasureRollButton);
+    tools.sort((a, b) => (a.sort || 0) - (b.sort || 0));
+    console.log("Treasure Roll button added to the toolbar:", tools);
+  });
+  */
 });
