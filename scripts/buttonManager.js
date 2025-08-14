c98d7b656d816821c43a2df603ab2ecb8165b286
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-08-14 12:49:53 -0500

    created tools function

--- scripts/buttonManager.js
+++ scripts/buttonManager.js
@@ -1,19 +1,23 @@
-Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
-  const lookfarTools = [
+function getLookfarTools() {
+  return [
     {
+      // Travel Check
       name: 'LOOKFAR.Button.TravelCheck.Name',
       icon: 'fa-solid fa-person-hiking',
       onClick: () => Hooks.call('lookfarShowTravelCheckDialog'),
-      __lookfar: true,
-      sort: Number.MAX_SAFE_INTEGER
-    }
+    },
+
+    // {
+    //   // Example placeholder for a future feature
+    //   name: 'LOOKFAR.Button.AnotherFeature.Name',
+    //   icon: 'fa-solid fa-compass',
+    //   onClick: () => Hooks.call('lookfarShowAnotherFeature'),
+    // },
   ];
+}
 
-  for (const t of lookfarTools) tools.push(t);
-
-  // Force Lookfar items to the end, keep relative order of others
-  const others = tools.filter(t => !t.__lookfar);
-  const ours   = tools.filter(t =>  t.__lookfar);
-  tools.length = 0;              // mutate the same array object
-  tools.push(...others, ...ours);
+// Hook into the same FU toolbar hook and push all our tools.
+Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
+  for (const tool of getLookfarTools()) tools.push(tool);
+  return tools;
 });
