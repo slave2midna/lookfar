6cf8ce0b06a321563543acd31887f8d5b110cbc6
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-08-14 12:22:58 -0500

    ready for v13

--- scripts/buttonManager.js
+++ scripts/buttonManager.js
@@ -1,25 +1,12 @@
-Hooks.once("ready", () => {
-  console.log("Button Manager: Ready Hook Fired");
-
-  // Adds floating button for Travel Check into Project FU's toolbar
-  Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
-    console.log("Adding Travel Check button to toolbar...");
-
-    const travelCheckButton = {
-      name: game.i18n.localize("LOOKFAR.Button.TravelCheck.Name"),
-      title: game.i18n.localize("LOOKFAR.Button.TravelCheck.Title"),
-      icon: "fa-solid fa-person-hiking",
-      button: true,
-      onClick: () => {
-        console.log("Travel Check button clicked!");
-        Hooks.call("lookfarShowTravelCheckDialog");
-      },
-      visible: true,
-      sort: 9999
-    };
-
-    tools.push(travelCheckButton);
-    tools.sort((a, b) => (a.sort || 0) - (b.sort || 0));
-    console.log("Travel Check button added to the toolbar:", tools);
+Hooks.on(projectfu.SystemControls.HOOK_GET_SYSTEM_TOOLS, (tools) => {
+  tools.push({
+    // Use the i18n key, just like FU does with 'FU.AppMetaCurrencyTrackerTitle'
+    name: 'LOOKFAR.Button.TravelCheck.Name',
+    icon: 'fa-solid fa-person-hiking',
+    onClick: () => Hooks.call('lookfarShowTravelCheckDialog'),
   });
+
+  // If FU consumes the returned array (pure function style), this helps.
+  // If it mutates in-place, returning is harmless.
+  return tools;
 });
