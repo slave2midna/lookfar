596e9020789cf392d5182d032f89523784d84019
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-11-18 21:00:05 -0600

    dungeon builder

--- scripts/init.js
+++ scripts/init.js
@@ -1,18 +1,26 @@
 import "./cacheManager.js";
 import { LookfarSettings } from "./settings.js";
 import { dataLoader } from "./dataLoader.js";
+
 import "./travelRoll.js";
 import "./treasureRoll.js";
-import "./buttonManager.js";
 import "./itemForge.js";
+import "./dungeonBuilder.js";
+import "./buttonManager.js";
 
 Hooks.once("init", async () => {
   console.log("Lookfar GM Assistant: Initializing...");
+
+  // Load static data files (equipment, qualities, dangers, discoveries, etc.)
   await dataLoader.loadData();
+
+  // Register Lookfar settings
   LookfarSettings.registerSettings();
+
   console.log("Lookfar GM Assistant Loaded.");
 });
 
 Hooks.once("ready", () => {
+  // Register dynamic settings that require game ready
   LookfarSettings.registerDynamicRollTableSettings();
 });
